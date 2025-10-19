92fa73cbae3e169621f3b4939034eb3395f713d8
vsandhu-developer <developervsandhu@gmail.com>
2025-10-19 18:39:37 -0400

    initialized contract

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+entrypoint!(counter_contract);
+
+pub fn counter_contract(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    Ok(())
+}
