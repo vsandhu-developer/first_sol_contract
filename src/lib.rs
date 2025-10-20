3a18fc4c201857a4dc4e863aa9a3e53cec6ac310
vsandhu-developer <developervsandhu@gmail.com>
2025-10-19 20:55:47 -0400

    added basic contract

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,19 @@ use solana_program::{
     pubkey::Pubkey,
 };
 
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+enum InstructionType {
+    Increment(u32),
+    Decrement(u32),
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter {
+    count: u32,
+}
+
 entrypoint!(counter_contract);
 
 pub fn counter_contract(
@@ -13,5 +26,20 @@ pub fn counter_contract(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    let acc = next_account_info(&mut accounts.iter())?;
+
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+    match instruction_type {
+        InstructionType::Increment(value) => {
+            counter_data.count += value;
+        }
+        InstructionType::Decrement(value) => {
+            counter_data.count -= value;
+        }
+    }
+
+    counter_data.serialize(&mut *acc.data.borrow_mut())?;
     Ok(())
 }
